39b900374cc52160c7dd057b2e21f2afa52e743b
liuyicheng <liuyicheng@mucang.cn>
2021-11-25 10:19:09 +0800

    复现 push一个页面回来，页面会重新走build 路径 （ open flutter page => popUntil demo  ）

--- example/lib/case/popUntil.dart
+++ example/lib/case/popUntil.dart
@@ -53,9 +53,44 @@ class PopUntilRouteState extends State<PopUntilRoute> {
                         .push("popUntilView", withContainer: false);
                   },
                   child: Text('push without container ')),
+              TextButton(
+                  onPressed: () {
+                    BoostNavigator.instance.push("secondStateful", withContainer: true);
+                  },
+                  child: Text('push with container 复现 push 回来会rebuild')),
+              TextButton(
+                  onPressed: () {
+                    BoostNavigator.instance.push("secondStateful", withContainer: false);
+                  },
+                  child: Text('push without container 复现 push 回来会rebuild')),
+              FutureBuilder<String>(
+                future: mockNetworkData(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  // 请求已结束
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      // 请求失败，显示错误
+                      return Text("Error: ${snapshot.error}");
+                    } else {
+                      // 请求成功，显示数据
+                      return Text("Contents: ${snapshot.data}");
+                    }
+                  } else {
+                    // 请求未结束，显示loading
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
             ],
           )),
         ));
+  }
+
+  int current= 0;
+  Future<String> mockNetworkData() async {
+    debugPrint("futurebuilder Page A ${PopUntilRoute.count}");
+    current++;
+    return Future.delayed(Duration(seconds: 2), () => "我是build的次数: ${current}");
   }
 
   void onBackPressed(BuildContext context) {
